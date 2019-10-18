37169b96ac80d7559786d0f8f168d882dc5f7614
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-18 11:49:53 -0500

    dad joke tests pass

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -26,23 +26,56 @@ describe('POST /register', () => {
 
 })
 
-describe('GET /', () => {
+describe('GET /jokes', () => {
     beforeEach(async() => {
         await db('users')
             .truncate();
     })
 
-    it('should register member to db', async () => {        
-        const response = await request(server).get('/api/jokes')
-            expect(response.status).toBe(201)
+    it('should allow authenticated users to view jokes in JSON format', async () => {    
+        //user registers
+        const response = await request(server).post('/api/auth/register')
+        .send({ username: 'galadriel', password: 'pippin' })
         
+        // expect(response.body.password === 'pippin').toBeFalsy()//password is hashed
+
+        //user logs in
+        const loginResponse = await request(server).post('/api/auth/login')
+            .send({ username: 'galadriel', password: 'pippin' })
+            .expect('Content-Type', /json/)
+        console.log(loginResponse.body)
+        //server responds with jokes in json format
+        const dadJokes = await request(server).get('/api/jokes')
+            .auth('galadriel', 'pippin')
+            .set('Authorization', loginResponse.body.token)
+            .expect('Content-Type', /json/)
+
     })
 
-    // it('should return a status of 201', async () => {
-    //     const response = await request(server).post('/api/auth/register')
-    //     .send({ username: 'galadriel', password: 'pippin' })
-    //     expect(response.status).toBe(201)
-    // })
+    it('jokes should be bad jokes', async () => {    
+        //user registers
+        const response = await request(server).post('/api/auth/register')
+        .send({ username: 'galadriel', password: 'pippin' })
+        
+        // expect(response.body.password === 'pippin').toBeFalsy()//password is hashed
+
+        //user logs in
+        const loginResponse = await request(server).post('/api/auth/login')
+            .send({ username: 'galadriel', password: 'pippin' })
+            .expect('Content-Type', /json/)
+        console.log(loginResponse.body)
+        //server responds with dad jokes
+        const dadJokes = await request(server).get('/api/jokes')
+            .auth('galadriel', 'pippin')
+            .set('Authorization', loginResponse.body.token)
+            .expect('Content-Type', /json/)
+
+        expect(dadJokes.body[0]).toEqual({
+            id: '0189hNRf2g',
+            joke: "I'm tired of following my dreams. I'm just going to ask them where they are going and meet up with them later."
+          })
+
+    })
 
 })
 
